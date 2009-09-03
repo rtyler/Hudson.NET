85c82cb178b85079c78532e3720e4cda8693e273
R. Tyler Ballance <tyler@monkeypox.org>
2009-09-02 23:52:13 -0700

    Add support for building some Hudson.NET samples

---
+++ Samples/InteractiveCLI.cs
@@ -0,0 +1,17 @@
+using System;
+
+using Hudson;
+
+namespace Hudson.Samples
+{
+	public class InteractiveCLI
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("");
+			Console.WriteLine("=> Welcome to the Hudson.NET interactive CLI!");
+			Api api = new Api();
+
+		}
+	}
+}
