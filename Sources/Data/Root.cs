5f7363db4ece87b074a562e50757c8ee2759ca7a
R. Tyler Ballance <tyler@monkeypox.org>
2009-09-02 23:06:46 -0700

    Add a Root deserialization class for the 'root' api response from hudson/api/json

---
+++ Sources/Data/Root.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Web.Script.Serialization;
+
+namespace Hudson.Data
+{
+	[Serializable]
+	public class Root
+	{
+		#region "Member Variables"
+		protected string mode = null;
+		protected List<Project> jobs = null;
+		protected string nodeDescription = null;
+		protected int numExecutors = 0;
+		protected string description = null;
+		#endregion
+
+		#region "Public Properties"
+		public string Mode
+		{
+			get { return this.mode; }
+			set { this.mode = value; }
+		}
+
+		public List<Project> Jobs
+		{
+			get { return this.jobs; } 
+			set { this.jobs = value; }
+		}
+
+		public string NodeDescription
+		{
+			get { return this.nodeDescription; }
+			set { this.nodeDescription = value; }
+		}
+
+		public int NumExecutors
+		{
+			get { return this.numExecutors; }
+			set { this.numExecutors = value; }
+		}
+		
+		public string Description
+		{
+			get { return this.description; }
+			set { this.description = value; }
+		}
+		#endregion
+	}
+}
---
+++ Tests/Data/RootTest.cs
@@ -0,0 +1,34 @@
+using System;
+using System;
+using System.Collections.Generic;
+using System.Web.Script.Serialization;
+
+using NUnit.Framework;
+
+using Hudson.Data;
+
+namespace Hudson.Tests.RootTests
+{
+	[TestFixture]
+	public class RootTest : Hudson.Tests.TestBase
+	{
+		protected readonly string sampleRoot = "{\"assignedLabels\":[{}],\"mode\":\"NORMAL\",\"nodeDescription\":\"the master Hudson node\",\"nodeName\":\"\",\"numExecutors\":2,\"description\":null,\"jobs\":[{\"name\":\"Downstream\",\"url\":\"http://localhost:8080/job/Downstream/\",\"color\":\"blue\"},{\"name\":\"Hudson.NET\",\"url\":\"http://localhost:8080/job/Hudson.NET/\",\"color\":\"red\"},{\"name\":\"Sleeper\",\"url\":\"http://localhost:8080/job/Sleeper/\",\"color\":\"blue\"}],\"primaryView\":{\"name\":\"All\",\"url\":\"http://localhost:8080/\"},\"slaveAgentPort\":0,\"useCrumbs\":false,\"useSecurity\":false,\"views\":[{\"name\":\"All\",\"url\":\"http://localhost:8080/\"},{\"name\":\"TestView\",\"url\":\"http://localhost:8080/view/TestView/\"}]}";
+
+		[Test]
+		public void BasicDeserialization()
+		{
+			Root root = this.json.Deserialize<Root>(this.sampleRoot);
+			Assert.IsNotNull(root, "Deserialized Rootobject is null");
+		}
+
+		[Test]
+		public void VerifyMembers()
+		{
+			Root root = this.json.Deserialize<Root>(this.sampleRoot);
+			Assert.IsNotNull(root, "Deserialized Rootobject is null");
+
+			Assert.IsNotNull(root.Jobs, "Jobs member is null");
+			Assert.AreEqual("NORMAL", root.Mode, "Mode is not NORMAL");
+		}
+	}
+}
