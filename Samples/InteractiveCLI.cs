671a7e950891e0eaed39442bebe61302794f7803
R. Tyler Ballance <tyler@monkeypox.org>
2009-09-02 23:55:34 -0700

    Add a FetchProjects() call to the sample

--- Samples/InteractiveCLI.cs
+++ Samples/InteractiveCLI.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 
 using Hudson;
+using Hudson.Data;
 
 namespace Hudson.Samples
 {
@@ -12,6 +14,19 @@ namespace Hudson.Samples
 			Console.WriteLine("=> Welcome to the Hudson.NET interactive CLI!");
 			Api api = new Api();
 
+			Console.WriteLine("Pinging your Hudson instance...");
+			List<Project> projects = api.FetchProjects();
+
+			if ( (projects == null) || (projects.Count == 0) )
+			{
+				Console.WriteLine("Looks like Hudson is empty :(");
+				return;
+			}
+
+			foreach (Project proj in projects) 
+			{
+				Console.WriteLine("  {0}\t{1}", proj.Name, proj.Color);
+			}
 		}
 	}
 }
